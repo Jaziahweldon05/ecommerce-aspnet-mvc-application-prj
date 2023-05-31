6f7198840ba738c059ad673a7cd91867aa9991ca
Jaziahweldon05 <jaziah.weldon@students.cau.edu>
2023-05-31 16:12:54 -0400

    creating database and wiring up back frameworks

---
+++ eTickets/Data/Enums/MovieCategory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Enums
+{
+    public enum MovieCategory
+    {
+        Action = 1,
+        Comedy,
+        Drama,
+        Documentary
+    }
+}
