ded315c5853b5e17223cce6b82912d9e3d34f529
Jaziahweldon05 <jaziah.weldon@students.cau.edu>
2023-08-03 16:19:21 -0400

    Model validation

--- eTickets/Controllers/ActorsController.cs
+++ eTickets/Controllers/ActorsController.cs
@@ -1,10 +1,12 @@
 ﻿using eTickets.Data;
 using eTickets.Data.Services;
+using eTickets.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 
 namespace eTickets.Controllers
 {
@@ -21,6 +23,23 @@ namespace eTickets.Controllers
         {
             var data = await _service.GetAll();
              return View(data);
+        }
+
+        //GetUrl: Actors/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("FullName, ProfilePictureURL, Bio")]Actor actor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(actor);
+            }
+            _service.Add(actor);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
